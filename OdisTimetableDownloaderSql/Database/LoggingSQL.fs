82fafa6d7c22d280ce8dab01684e156a5415714e
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2024-03-22 18:53:59 +0100

    22-03-2024

--- OdisTimetableDownloaderSql/Database/LoggingSQL.fs
+++ OdisTimetableDownloaderSql/Database/LoggingSQL.fs
@@ -30,5 +30,7 @@ module LoggingSQL =
             |> List.distinct        
         with
         | ex -> 
-              logInfoMsg <| sprintf "102 %s" (string ex.Message)                        
+              printfn "%s" "Je třeba zavolat programátora, tato chyba není zaznamenána v log file. Error 2001."
+              printfn "%s" <| string ex.Message //proste s tim nic nezrobime, kdyz to nebude fungovat... 
+              logInfoMsg <| sprintf "102 %s" (string ex.Message) //zkusime to...                        
               [] //tady nevadi List.empty jakozto vystup 
